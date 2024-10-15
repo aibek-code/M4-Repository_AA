4d080e92bd2d914efe4db02cafd7bb2e8288e50c
aibek-code <aibekabasov70@gmail.com>
2024-10-15 16:55:47 -0500

    Add files via upload

---
+++ pirate.py
@@ -0,0 +1,7 @@
+# Aibek Abasov
+# 10/15/2024
+greeting = input("Hello, possible pirate! What's the password?")
+if greeting ==("Arrr!"):
+	print("Greetings, hater of pirates!")
+else:
+	print("Go away, pirate.")
---
+++ time.py
@@ -0,0 +1,10 @@
+# Aibek Abasov
+# 10/15/2024
+currentTimeStr = input("What is the current time (in hours 0-23)?")
+waitTimeStr = input("How many hours do you want to wait?")
+
+currentTimeInt = int(currentTimeStr)
+waitTimeInt = int(waitTimeStr)
+
+finalTimeInt = currentTimeInt + waitTimeInt
+print(finalTimeInt)
